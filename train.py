e10964adad29bd7b09fa6db1f2783051ed6adc1f
misganaOSS <55154583+misganaOSS@users.noreply.github.com>
2020-10-15 07:01:46 +0000

    updated version

--- train.py
+++ train.py
@@ -16,12 +16,10 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 
 ds =TabularDatasetFactory.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv") ### YOUR CODE HERE ###
 
-x, y = clean_data(ds)
 
-# TODO: Split data into train and test sets.
 
-### YOUR CODE HERE ###a
-x_train,x_test,y_train,y_test = train_test_split(x, y,test_size=0.3,random_state=42)
+
+
 run = Run.get_context()
 
 def clean_data(data):
@@ -49,7 +47,13 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+    return x_df,y_df
+
+# TODO: Split data into train and test sets.
+
+### YOUR CODE HERE ###
+x, y = clean_data(ds)
+x_train,x_test,y_train,y_test = train_test_split(x, y,test_size=0.3,random_state=42)    
 
 def main():
     # Add arguments to script
@@ -69,4 +73,5 @@ def main():
     run.log("Accuracy", np.float(accuracy))
 
 if __name__ == '__main__':
+
     main()
